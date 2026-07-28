83fa94984665330d5802b63a42751a95c65244a0
Tsukiyo07 <110260198+Tsukiyo07@users.noreply.github.com>
2026-07-28 21:30:30 +0200

    Update api/tts.js (correction bug UI)

--- api/tts.js
+++ api/tts.js
@@ -1,3 +1,5 @@
+import { Buffer } from 'buffer';
+
 export default async function handler(req, res) {
     if (req.method !== 'GET' && req.method !== 'POST') {
         return res.status(405).json({ error: "Method not allowed" });
