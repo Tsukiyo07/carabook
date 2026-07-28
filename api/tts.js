dd6b32323a006a73a2d5bef79aefe55ccf73a3f0
Tsukiyo07 <110260198+Tsukiyo07@users.noreply.github.com>
2026-07-28 21:31:40 +0200

    Update api/tts.js (correction bug UI)

--- api/tts.js
+++ api/tts.js
@@ -1,6 +1,4 @@
-import { Buffer } from 'buffer';
-
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     if (req.method !== 'GET' && req.method !== 'POST') {
         return res.status(405).json({ error: "Method not allowed" });
     }
@@ -35,4 +33,4 @@ export default async function handler(req, res) {
         console.error("TTS Proxy Error:", error);
         res.status(500).json({ error: "TTS Proxy Error: " + error.message });
     }
-}
+};
